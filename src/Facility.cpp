5bcfed59e6cd903d81e5976ddbba023bd704ccb7
GuyStein1 <guy.stt8@gmail.com>
2024-11-23 09:43:28 +0000

    Finished implementing Facilty.cpp

--- src/Facility.cpp
+++ src/Facility.cpp
@@ -39,16 +39,42 @@ FacilityCategory FacilityType :: getCategory() const {
 Facility :: Facility(const string &name, const string &settlementName, const FacilityCategory category,
                    const int price, const int lifeQuality_score, const int economy_score, const int environment_score)
     : FacilityType(name, category, price, lifeQuality_score, economy_score, environment_score),
-      settlementName(settlementName), status(FacilityStatus::UNDER_CONSTRUCTIONS), timeLeft(0) {}
+      settlementName(settlementName), status(FacilityStatus::UNDER_CONSTRUCTIONS), timeLeft(price) {}
 
 Facility :: Facility(FacilityType &type, const string &settlementName)
-    : FacilityType(type), settlementName(settlementName), status(FacilityStatus::UNDER_CONSTRUCTIONS), timeLeft(0) {}
+    : FacilityType(type), settlementName(settlementName), status(FacilityStatus::UNDER_CONSTRUCTIONS), timeLeft(price) {}
 
 
+const string &Facility::getSettlementName() const {
+    return settlementName;
+}
 
+const FacilityStatus &Facility::getStatus() const {
+    return status;
+}
 
+const int Facility::getTimeLeft() const {
+    return timeLeft;
+}
 
+void Facility::setStatus(FacilityStatus status){
+    this->status = status;
+}
 
+FacilityStatus Facility::step() {
+    if (timeLeft > 0) {
+        timeLeft--;
+        if (timeLeft == 0) {
+            status = FacilityStatus::OPERATIONAL;
+        }
+    }
+    return status;
+}
+
+const string Facility::toString() const {
+    string statusStr = (status == FacilityStatus::UNDER_CONSTRUCTIONS) ? "Under Construction" : "Operational";
+    return "Facility(Name: " + getName() + ", Settlement: " + settlementName + ", Status: " + statusStr + ")";
+}
 
 //Helper function to convert int to FacilityCategory
 FacilityCategory createFacilityCategory(int value) {
