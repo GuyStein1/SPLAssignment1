e95f43e3a3e48f5f04198c5013f72688a552321d
GuyStein1 <guy.stt8@gmail.com>
2024-11-23 08:46:05 +0000

    implmented settlment

--- src/Settlement.cpp
+++ src/Settlement.cpp
@@ -1,15 +1,38 @@
 #include "Settlement.h"
 #include <iostream>
 
+Settlement ::Settlement(const string &name, SettlementType type)
+    : name(name), type(type) {}
 
+const string &Settlement :: getName() const {
+    return name;
+}
 
-SettlementType createSettlementType(int value) {
-    switch (value) {
-        case 0: return SettlementType::VILLAGE;
-        case 1: return SettlementType::CITY;
-        case 2: return SettlementType::METROPOLIS;
-        default:
-            throw std::invalid_argument("Invalid value for SettlementType");
+SettlementType Settlement :: getType() const {
+    return type;
+}
+
+const string Settlement :: toString() const {
+    if (type == SettlementType :: VILLAGE) {
+        return "Settlement(Name: " + name + ", Type: VILLAGE)";
+    } else if (type == SettlementType :: CITY){
+        return "Settlement(Name: " + name + ", Type: CITY)";
+    } else {
+        return "Settlement(Name: " + name + ", Type: METROPOLIS)";
     }
 }
 
+SettlementType createSettlementType(int value)
+{
+    switch (value)
+    {
+    case 0:
+        return SettlementType::VILLAGE;
+    case 1:
+        return SettlementType::CITY;
+    case 2:
+        return SettlementType::METROPOLIS;
+    default:
+        throw std::invalid_argument("Invalid value for SettlementType");
+    }
+}
