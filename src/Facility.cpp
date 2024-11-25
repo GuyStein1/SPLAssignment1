384e6bea12810b943611b531c4a7f2826e4e2b50
GuyStein1 <guy.stt8@gmail.com>
2024-11-25 13:03:14 +0000

    fixed facility signature

--- src/Facility.cpp
+++ src/Facility.cpp
@@ -41,7 +41,7 @@ Facility :: Facility(const string &name, const string &settlementName, const Fac
     : FacilityType(name, category, price, lifeQuality_score, economy_score, environment_score),
       settlementName(settlementName), status(FacilityStatus::UNDER_CONSTRUCTIONS), timeLeft(price) {}
 
-Facility :: Facility(FacilityType &type, const string &settlementName)
+Facility :: Facility(const FacilityType &type, const string &settlementName)
     : FacilityType(type), settlementName(settlementName), status(FacilityStatus::UNDER_CONSTRUCTIONS), timeLeft(price) {}
 
 
